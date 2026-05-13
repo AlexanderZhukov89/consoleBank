5beff4dc1681ef79a78cc052fd4667596e633606
Alexander <grandalexander89@gmail.com>
2026-05-13 13:57:44 +0300

    first commit

---
+++ src/test/java/com/example/consoleBank/ConsolBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consoleBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
